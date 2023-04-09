f09ee1fa62c18a4892b7957dd6bd2caeeafd909a
tanvhla258 <tanvhla258@gmail.com>
2023-04-09 17:47:16 +0700

    delete env webpack

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv-webpack";
 
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  // },
-  // build: {
-  //   outDir: "build",
-  // },
-  // optimizeDeps: {
-  //   include: ["react", "react-dom"],
-  // },
 });
